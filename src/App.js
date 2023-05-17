591e67fd13a1cdfe183280a7a7c5aa6c4ad53e25
Enes Yağmur <106833747+enesrain@users.noreply.github.com>
2023-05-17 23:40:21 +0300

    kabaca gorunum yapildi ve bootstrap kuruldu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div>
+          Random Number:<span>x</span>
+        </div>
+        <div>
+          <input type="text" placeholder="Min değer giriniz" />
+          <input type="text" placeholder="Max değer giriniz" />
+        </div>
+        <button>Get random number</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
